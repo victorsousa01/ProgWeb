316c37bcf8d9c3d89025fcfa24e22348dc9f3b09
Victor Quintanilha <vqs@icomp.ufam.edu.br>
2017-06-16 17:29:44 -0400

    Exercicios de PHP

---
+++ PHP3/PHP_impressao.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Nome: " . $_GET["nome"] . "<br>";
+	echo "Email: " . $_GET["email"] . "<br>";
+	echo "Website: " . $_GET["website"] . "<br>";
+	echo "Mensagem: " . $_GET["mensagem"] . "<br>";
+?>
